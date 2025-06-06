cbebc244ad67cd83b57eff7c32d4d1faac6e20d6
Luke <lukedavies@gmail.com>
2025-06-06 20:40:39 +0100

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const abcWhyte = localFont({
+  src: "../../public/static/ABCWhyteVariable.woff2",
+  variable: "--font-abc-whyte",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${abcWhyte.variable} antialiased`}
       >
         {children}
       </body>
